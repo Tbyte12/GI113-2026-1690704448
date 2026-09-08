a4e1f4a960704d44aeff66256b18aca4fb30eeb6
Tbyte12 <sakarinasawamg@gmail.com>
2026-09-08 16:18:23 +0700

    Added Assignment 1
    Unfinished

---
+++ GI113_129D_Assignment 1/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace GI113_129D_Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string PlayerStat = "Career Statistics";
+
+            var freedomFighter = "Thessaloniki";
+            var fighterRank = "Lieutenant";
+            var fighterLevel = 57;
+            int killTotal = 140035;
+            int robotKillTotal = 10326;
+            int incapacitation = 532;
+            float Accuracy = 94.75f;
+            float killPerIncapacitation = 24.22f;
+            int friendlyKill = 423;
+            double friendlyFireTotal = 40640;
+            double missionSuccess = 64.2;
+            int missionFailure = 564;
+
+            //---------------------------------------------
+
+
+
+        }
+    }
+}
