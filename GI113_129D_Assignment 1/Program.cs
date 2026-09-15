2d64f505f8b758d6a003583e54ccf25e1ff20a86
Tbyte12 <sakarinasawamg@gmail.com>
2026-09-16 00:26:36 +0700

    Added Assignment 1 (1)

--- GI113_129D_Assignment 1/Program.cs
+++ GI113_129D_Assignment 1/Program.cs
@@ -4,24 +4,49 @@
     {
         static void Main(string[] args)
         {
-            const string PlayerStat = "Career Statistics";
-
-            var freedomFighter = "Thessaloniki";
+            const string PlayerStat = "MMP\"\"\"\"\"\"\"MM                              dP  \r\nM' .mmmm  MM                              88  \r\nM         `M .d8888b. .d8888b. 88d888b. d8888P\r\nM  MMMMM  MM 88'  `88 88ooood8 88'  `88   88  \r\nM  MMMMM  MM 88.  .88 88.  ... 88    88   88  \r\nM  MMMMM  MM `8888P88 `88888P' dP    dP   dP  \r\nMMMMMMMMMMMM      .88                         \r\n              d8888P                          \r\nMP\"\"\"\"\"\"`MM   dP              dP              \r\nM  mmmmm..M   88              88              \r\nM.      `YM d8888P .d8888b. d8888P            \r\nMMMMMMM.  M   88   88'  `88   88              \r\nM. .MMM'  M   88   88.  .88   88              \r\nMb.     .dM   dP   `88888P8   dP              \r\nMMMMMMMMMMM                                   ";
+            string freedomFighter = "Thessaloniki";
             var fighterRank = "Lieutenant";
             var fighterLevel = 57;
             int killTotal = 140035;
             int robotKillTotal = 10326;
-            int incapacitation = 532;
-            float Accuracy = 94.75f;
-            float killPerIncapacitation = 24.22f;
+            int incapTotal = 532;
+            double hitAccuracy = 94.752349;
+            float killPerDeath = 24.22f;
             int friendlyKill = 423;
             double friendlyFireTotal = 40640;
             double missionSuccess = 64.2;
             int missionFailure = 564;
 
-            //---------------------------------------------
+            //-------------------------------------------------------
 
+            Console.WriteLine(PlayerStat);
+            Console.WriteLine("\n----------------------------------------------------");
+            Console.WriteLine($"Agent:                      {freedomFighter}");
+            Console.WriteLine($"Ranking:                    {fighterRank}");
+            Console.WriteLine($"Experience Level:           {fighterLevel}");
 
+            Console.WriteLine("----------------------------------------------------");
+
+            double killTotalLeniant = killTotal;
+            Console.WriteLine($"Enemy Incapacitated:        {killTotal}");
+            Console.WriteLine($"Machine Destroyed:          {robotKillTotal}");
+            Console.WriteLine($"Total Incapacitation:       {incapTotal}");
+            
+            float hitAccuracyRounded = (int)hitAccuracy;
+            Console.WriteLine($"Fired Round Accuracy:       {hitAccuracyRounded}");
+
+            float killPerDeathRounded = (int)killPerDeath;
+            Console.WriteLine($"Kill Per Incapacitation:    {killPerDeathRounded}");
+            Console.WriteLine($"Operational Success:        {missionSuccess}");
+            Console.WriteLine($"Operational Failure:        {missionFailure}");
+           
+            Console.WriteLine($"\n============== Operational Misconducts =============");
+            Console.WriteLine($"\nCount of Ally Manslaughter: {friendlyKill} Charges");
+            Console.WriteLine($"Count of Ally Assault:      {friendlyFireTotal} Charges");
+            Console.WriteLine("\n================= Agent Will Be Charge ACCORDINGLY =");
+
+            Console.WriteLine("\n\n. ....  .    .  ..         . .      ..=+:.....        \r\n  .-:.....                       ....:#*...*-..   .   \r\n  ..+#:....   .   ...             ...#@=..+@-..       \r\n  ...*@++:..   .  ...      .. .   ..*@+..*@#:... .  . \r\n  ...:@@%%=..  .  ... .       .....*@#:.+@%-.=:.  .   \r\n  ....+@@@%=:.... ...         ...-%@%-.+@%-.=*:...    \r\n.    .-@@@@@#+:.....       ....:+@@%=-#@%-.-%+....    \r\n .  ...#@@@@@%+*...    .......=%@@@**@@#:.=%*:....  . \r\n    ...*@@@@@@@%=.... .....:=%@@@@@@@@*::*@*:....     \r\n    ...*@@@@@@@@%-... ...:+@@@@@@@@@@+-#@@+.:+....  . \r\n     .-@@@@@@@@@@@*.. ..=@@@@@@@@@@@@@@@%-.=%-...     \r\n    ..-@@@@@@@@@@@@#:..=@@@@@@@@@@@@@@@+:=#*:...    . \r\n  . ...=@@@@@@@@@@@@#:-#@@@@@@@@@@@@@@@%@#-... ..  .  \r\n  . ....+@@@@@@@@@@@@*=@@@@@@@@@@@@@@@@%=:=:..   .. . \r\n .. .....#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#**:...     .  \r\n     ....-@@@@@@@@@@@@@@@@@@@@@@@@@@@@@#-.......      \r\n      ....#@@@@@@@@@@@@@@@@@@@@@@@@@@@@#:..        .  \r\n     .  ..+@@@@@@@@@@@@@@@@@@@@@@@@@@@#:...           \r\n        ..-@@@@@@@@@@@@@@@@@@@@@@@@@@@=...   .. .     \r\n. .  .....=@@@@@@@@@@@@@@@@@@@@@@@@@@#:...            \r\n     ...:#@@@@@@@@@@@@@@@@@@@@@@@@@%=...              \r\n    ...:@@@@@@@@@@@@@@@@@@@@@@@@@#+:...               \r\n . ....#@@@@@@@@@@@@@@@@@@@@@@@@#-....                \r\n  ....*@@@@@@@@@@@@@@@@@@@@@@@@@@%-...                \r\n  ...:@@@@@@@@@@@@@@@@@@@@@@@@@@@@@=...           .   \r\n  ...+@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@=..  . ..   .     \r\n  ...#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@=..              \r\n  ..:@@@@@@@@@@#:...:-:-*@@@@@@@@@@@@@=....      .    \r\n  ...%@@@@@@@@*:........=@@%@@@@#-.:#@@=...           \r\n  ...=@@@@@@@*........::+@*-#%%@%=..=@@@=...      . . \r\n   ...+@@@@@%:..-%@@%%###*=*@%+*@+...*@@@=....  .     \r\n    ...=@@@@#.:*@#**....-#%-..*#-.....#@@@=....       \r\n    .....=@@#.:*=*#=.=%@+:..-#+.......+@@@@-..        \r\n       ..=@@%:.:-=:.=@#++.=%@+..     ..#@@@@-.        \r\n        .:%@%-.....:*@*=+:=@*++...  ...+@@@@#:....    \r\n      .....==.  .  .:**+:.-#*=-..... ..=%@@@@#:...  . \r\n .       ..... ..  ........-=-...    ..:#@@@@@*:..    \r\n                .   .                ...=%@@@@@+..    \r\n   ...                  .            ....+%@@@@@+.... \r\n           .  .    .     .     . .   . ....=%@@@@-... \r\n                             .        .  ...:-%%@%:...\r\n .      ..  .    .                       .   ..:.++...\r\n   .               .        .                .........");
 
         }
     }
